7efb8e1060a8891af212228a1bbe11bb37f54f2f
Kattis91 <katya.svedmark@gmail.com>
2024-10-06 13:38:06 +0200

    start

---
+++ components/CalcButton.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+type CalcButtonProps = { buttontext: string, isActive : boolean }
+
+export function CalcButton({buttontext, isActive} : CalcButtonProps) {
+    return(
+
+      <View style={{ flex: 1, margin: 20, backgroundColor: isActive ? "blue" : "red" }}>
+        <Text style={{ color: isActive ? "white" : "black" }}>{ buttontext }</Text>
+      </View>
+    );
+  }
